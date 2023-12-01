9ec76e03d99aaf5c88fef0a8a118f547fd311a2c
ELSOLRA <bardakasmiljo@gmail.com>
2023-12-01 11:28:13 +0100

    description styling, lines, font, color, structure

--- script/main.js
+++ script/main.js
@@ -11,9 +11,18 @@
 }); */
 
 const solarSystem = document.querySelector('.solar-system');
+const descriptionContainer = document.querySelector('.planet-description');
 const apiUrl = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com';
-
-let cachedData = {};
+const planetNameMap = {
+  mercury: 'Merkurius',
+  venus: 'Venus',
+  earth: 'Jorden',
+  mars: 'Mars',
+  jupiter: 'Jupiter',
+  saturn: 'Saturnus',
+  uranus: 'Uranus',
+  neptune: 'Neptunus'
+};
 const planetColors = {
   mercury: '136, 136, 136',
   venus: '231, 205, 205',
@@ -25,9 +34,23 @@ const planetColors = {
   neptune: '122, 145, 167'
 };
 
+let isDataFetched = false;
+let cachedData = {};
+
 
 
 // Function to fetch API key from the server
+
+(function init() {
+  // Attach click event listeners to each planet
+  const planets = document.querySelectorAll('.planet');
+  planets.forEach(planet => {
+      planet.addEventListener('click', () => {
+        const planetId = planet.id;
+        openOverlay(planetId);
+      });
+  });
+})();   // init function is invoked immediately!
 
 async function getApiKey() {
   
@@ -102,50 +125,90 @@ function getRandom(min, max) {
 }
 
 // Function to create stars and set their positions
-function createStars(parentElement) {
+async function createStars(parentElement) {
+  const starTypes = [
+    { type: 'star1', count: 15 },   //6x6
+    { type: 'star2', count: 22 },   //3x3
+    { type: 'star3', count: 12 },   //5.8x5.8
+    { type: 'star4', count: 2 }     //3x2
+  ];
   const numberOfStars = 51; 
 
-  for (let i = 0; i < numberOfStars; i++) {
-    const star = document.createElement('div');
-    star.className = 'star';
-    star.style.top = `${getRandom(0, window.innerHeight)}px`;
-    star.style.left = `${getRandom(0, window.innerWidth)}px`;
-    parentElement.append(star);
+  for (const { type, count } of starTypes) {
+    for (let i = 0; i < count; i++) {
+      const star = document.createElement('div');
+      star.className = `star ${type}`;
+      star.style.top = `${getRandom(0, window.innerHeight)}px`;
+      star.style.left = `${getRandom(0, window.innerWidth)}px`;
+      parentElement.append(star);
+    }
   }
 }
 
-function openOverlay(planet) {
+
+
+async function openOverlay(planet) {
+
+  console.log('Planet:', planet);
   
   const overlay = document.getElementById('overlay');
   const sun = document.getElementById('overlay-sun');
-  
+  const stars = document.querySelectorAll('.star');
 
-  const planetColor = planetColors[planet.toLowerCase()];
+  const planetColorName = planet.toLowerCase();
+  const planetColor = planetColors[planetColorName];
+  
+  console.log('Planet color name:', planetColorName, 'Planet color:', planetColor);
   
   sun.style.backgroundColor = `rgba(${planetColor}, 1)`;
   sun.style.boxShadow = `0 0 0 3.875rem rgba(${planetColor}, 0.1), 0 0 0 7.875rem rgba(${planetColor}, 0.06)`;
+  
 
-  const stars = document.querySelectorAll('.star');
 if (stars.length === 0) {
-  createStars(overlay);
+  await createStars(overlay);
 }
 
 solarSystem.style.display = 'none';
 overlay.style.display = 'flex';
 
+try {
+  // Fetch the data only if it has not been fetched before
+  if (!isDataFetched) {
+    const apiKey = await getApiKey();
+    const data = await fetchData(apiUrl, apiKey);
+    cachedData = data;
+    isDataFetched = true; //  the flag to true to avoid repeated fetching
+  }
+  
+
+  console.log('Planet:', planet);
+  console.log('Clicked planet:', planet);
+  console.log('Cached data:', cachedData);
+  console.log('Bodies array:', cachedData.bodies);
+
+  
+  const planetInfo = cachedData.bodies.find(body => body.name.toLowerCase() === planetNameMap[planet].toLowerCase());
+  
+ 
+  
+
+
+  
+  if (planetInfo) {
+    
+    console.log('Found planetInfo:', planetInfo);
+     updatePlanetDescription(planetInfo);
+  } else {
+    console.error('Invalid planetInfo:', planetInfo);
+  }
+} catch (error) {
+  console.error('Error fetching or processing data:', error.message);
+}
+
 closeButton();
 }
 
-(function init() {
-  // Attach click event listeners to each planet
-  const planets = document.querySelectorAll('.planet');
-  planets.forEach(planet => {
-      planet.addEventListener('click', () => {
-          const planetId = planet.id;
-          openOverlay(planetId);
-      });
-  });
-})();   // init function is invoked immediately!
+
 
 
 function closeButton() {
@@ -163,3 +226,77 @@ function closeButton() {
   });
 }
  
+
+
+
+function updatePlanetDescription(planetInfo) {
+
+  if (!planetInfo) {
+    console.error('Invalid planetInfo:', planetInfo);
+    return;
+  }
+ 
+  descriptionContainer.textContent = '';
+  const { name, latinName, desc, circumference, distance, temp, moons } = planetInfo;
+
+  const nameElement = document.createElement('h1');
+  nameElement.textContent = `${name}`;
+  nameElement.classList.add('planet-description__title');
+
+  const latinNameElement = document.createElement('h3');
+  latinNameElement.textContent = `${latinName}`;
+  latinNameElement.classList.add('planet-description__subtitle');
+
+  const descElement = document.createElement('p');
+  descElement.textContent = `${desc}`;
+  descElement.classList.add('planet-info');
+
+  const lineElement1 = document.createElement('hr');
+  lineElement1.classList.add('separator');
+
+  const circumferenceElement = document.createElement('p');
+  circumferenceElement.textContent = `${circumference} km`;
+  circumferenceElement.classList.add('planet-info');
+
+  const distanceElement = document.createElement('p');
+  distanceElement.textContent = `${distance} km`;
+  distanceElement.classList.add('planet-info');
+
+  // Creating separate elements for day and night temperatures
+  const tempDayElement = document.createElement('p');
+  tempDayElement.textContent = `${temp.day} °C`;
+  tempDayElement.classList.add('planet-info');
+
+  const tempNightElement = document.createElement('p');
+  tempNightElement.textContent = `${temp.night} °C`;
+  tempNightElement.classList.add('planet-info');
+
+  const lineElement2 = document.createElement('hr');
+  lineElement2.classList.add('separator');
+
+  const moonsElement = document.createElement('p');
+  if (moons.length > 0) {
+    moonsElement.textContent = `${moons.join(', ')}`;
+  } else {
+    moonsElement.textContent = `Doesn't have any known moons`;
+  }
+  moonsElement.classList.add('planet-info');
+
+  descriptionContainer.append(
+    nameElement,
+    latinNameElement,
+    descElement,
+    lineElement1,
+    circumferenceElement,
+    distanceElement,
+    tempDayElement,
+    tempNightElement,
+    lineElement2,
+    moonsElement
+  );
+} 
+
+
+function displayNoResults() {
+  searchedContent.innerHTML = "<p>No results found for the given search term.</p>";
+}
